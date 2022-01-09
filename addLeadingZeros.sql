3df62a397edcc5a048df4dfcfa687716578d92c7
Nitin Raj Srivastava <96106142+nitindwhbi@users.noreply.github.com>
2022-01-09 18:27:54 +0530

    Create addLeadingZeros.sql

---
+++ addLeadingZeros.sql
@@ -0,0 +1,24 @@
+-- 3 ways to add leading zeros in SQL
+
+/* using to_char function */
+
+select catid,
+to_char(catid,'0000') as catid_1
+from category
+order by 1 desc;
+
+/* using case statement */
+select catid,
+case when length(catid)=1 then '000'||catid
+when length(catid)=2 then '00'||catid
+when length(catid)=3 then '0'||catid
+when length(catid)=4 then ''||catid
+end as catid_1
+from category
+order by 1 desc;
+
+/* using right function */
+select catid,
+right('0000'||catid,4) as catid_1
+from category
+order by 1 desc;
